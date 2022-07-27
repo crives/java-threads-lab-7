aec48affc88891321c46e3436e371941c993eafe
Caroline Rives <car.rives@gmail.com>
2022-07-27 15:29:26 -0400

    Complete Threads Lab 7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,44 @@ import java.util.Scanner;
 import java.util.concurrent.Callable;
 
 public class Main {
+
+    /*
+    Write a method called getCallableInteger that reads an integer
+    from the standard input and returns a Callable<Integer>.
+     */
     public static Callable<Integer> getCallableInteger() {
-        // your code here
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        Callable callable = () -> num;
+        return callable;
     }
 }
+//
+// ## WAY 2 ##
+//import java.util.Scanner;
+//import java.util.concurrent.Callable;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        while (scanner.hasNext()) {
+//            int num = scanner.nextInt();
+//        }
+//    }
+//
+//    /*
+//    Write a method called getCallableInteger that reads an integer
+//    from the standard input and returns a Callable<Integer>.
+//     */
+//    public static Callable<Integer> getCallableInteger(int num) {
+//        Callable callable = new Callable() {
+//            @Override
+//            public Object call() throws Exception {
+//                return num;
+//            }
+//        };
+//        return callable;
+//    }
+//}
